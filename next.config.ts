a54781cb9e4e9b2257ba8afdf9df0538887d30fb
Vojtěch Šebek <vojtik.seba@gmail.com>
2025-12-20 11:22:09 +0100

    Oprava JS config

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,6 @@ const nextConfig = {
     // !! Důležité: Ignoruje chyby typů při nahrávání !!
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 
 };
 
