112859cd9cf729ea64d9e4d9b54986ba4687d596
Vojtěch Šebek <vojtik.seba@gmail.com>
2026-01-14 15:34:41 +0100

    doplnění načtení typu kodu

--- components/inventory/ScannerModal.tsx
+++ components/inventory/ScannerModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Html5Qrcode } from 'html5-qrcode';
+import { Html5Qrcode, Html5QrcodeSupportedFormats } from 'html5-qrcode';
 import { useEffect, useRef, useState } from 'react';
 
 type ScannerModalProps = {
@@ -55,7 +55,19 @@ export default function ScannerModal({ isOpen, onClose, onScan }: ScannerModalPr
             }
 
             try {
-                const scanner = new Html5Qrcode(readerId);
+                const scanner = new Html5Qrcode(readerId, {
+                    formatsToSupport: [
+                        Html5QrcodeSupportedFormats.QR_CODE,
+                        Html5QrcodeSupportedFormats.EAN_13,
+                        Html5QrcodeSupportedFormats.EAN_8,
+                        Html5QrcodeSupportedFormats.CODE_128,
+                        Html5QrcodeSupportedFormats.CODE_39,
+                        Html5QrcodeSupportedFormats.UPC_A,
+                        Html5QrcodeSupportedFormats.UPC_E,
+                        Html5QrcodeSupportedFormats.UPC_EAN_EXTENSION
+                    ],
+                    verbose: false
+                });
                 scannerRef.current = scanner;
 
                 const config = {
